225c6147d3bd34a12f4eaedefc2acb446e166a47
Yuwei Sun <sunyuwei@cloudin.cn>
2017-01-31 10:52:02 +0800

    添加练习轮数

--- client/src/app/containers/App.js
+++ client/src/app/containers/App.js
@@ -12,33 +12,38 @@ const styles = {
 };
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       totalExerciseFudaCount: 10,
       exercisedFudaCount: 0,
+      exercisedCount: -1, // 经历了几轮练习
+      currentExerciseFudaIndex: 0,
       totalToriFudaCount: 10,
-      exerciseFudaList: [],
+      exerciseFudaIndexList: [],
       score: 0,
       exercisedFudaList: [],
       correctFudaList: [],
       wrongFudaList: [],
       highestScore: 0,
     };
+
+    this.generateExerciseFudaList = this.generateExerciseFudaList.bind(this);
   }
 
-  componentDidMount() {
-    this.generateExerciseFudaList.call(this);
+  componentWillMount() {
+    this.generateExerciseFudaList();
   }
 
   generateExerciseFudaList() {
-    console.log(this);
-    const exerciseFudaList =  Utils.randomArray(100, this.state.totalExerciseFudaCount);
     this.setState({
-      exerciseFudaList,
-      currentExerciseFudaId: exerciseFudaList[0] + 1
+      exercisedCount: this.state.exercisedCount + 1,
+    });
+    const exerciseFudaIndexList =  Utils.randomArray(100, this.state.totalExerciseFudaCount);
+    this.setState({
+      exerciseFudaIndexList,
+      currentExerciseFudaId: exerciseFudaIndexList[0] + 1
     });
   }
 
@@ -55,7 +60,7 @@ export default class App extends React.Component {
   }
 
   setExercisedFudaList() {
-    const exercisedFudaList = this.state.exerciseFudaList;
+    const exercisedFudaList = this.state.exerciseFudaIndexList;
     exercisedFudaList.push(this.state.currentExerciseFudaId);
     this.setState({exercisedFudaList});
   }
@@ -67,16 +72,34 @@ export default class App extends React.Component {
   }
 
   checkIfExerciseEnd() {
-
+    if (this.state.totalExerciseFudaCount === this.state.exercisedFudaCount + 1) {
+      this.setState({
+        exercisedFudaCount: 0,
+        currentExerciseFudaIndex: 0,
+        score: 0,
+        exercisedFudaList: []
+      });
+      this.generateExerciseFudaList();
+    } else {
+      this.setState({
+        currentExerciseFudaId: this.state.exerciseFudaIndexList[this.state.currentExerciseFudaIndex + 1] + 1,
+        currentExerciseFudaIndex: this.state.currentExerciseFudaIndex + 1
+      });
+    }
   }
 
   onFudaCorrect() {
-    this.setState({score: this.state.score + 10});
-    if (this.state.score > this.state.highestScore) {
-      this.setState({highestScore: this.state.score});
+    let score = this.state.score + 10;
+    this.setState({score: score});
+    if (score > this.state.highestScore) {
+      this.setState({highestScore: score});
     }
     this.setExercisedFudaList();
     this.setCorrectFudaList();
+    this.checkIfExerciseEnd();
+  }
+
+  onFudaWrong() {
     this.checkIfExerciseEnd();
   }
   
@@ -86,14 +109,18 @@ export default class App extends React.Component {
     if (parseInt(selectedFudaId) === this.state.currentExerciseFudaId) {
       this.onFudaCorrect();
     } else {
-      this.checkIfExerciseEnd();
+      this.onFudaWrong();
     }
   }
 
   render() {
     return (
       <div style={styles.container}>
-        <Aside />
+        <Aside
+          score={this.state.score}
+          highestScore={this.state.highestScore}
+          exercisedCount={this.state.exercisedCount}
+        />
         <Main
           fudaId={this.state.currentExerciseFudaId}
           toriFudaCount={this.state.totalToriFudaCount}
